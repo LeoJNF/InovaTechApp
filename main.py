33491c589c4b7227941dc0a42225a9de7a563711
LeoJNF <nascimentoferreira.leandro@gmail.com>
2023-09-08 00:34:15 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivymd.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
+from kivymd.uix.card import MDCard
+
+Window.size = (350, 580)
+
+
+class EsqueciSenha(MDCard):
+    def FecharRecSenha(self):
+        self.parent.remove_widget(self)
+
+
+class MenuLogin(MDCard):
+    pass
+
+
+class TelaLogin(FloatLayout):
+    def AbrirRecSenha(self):
+        self.add_widget(EsqueciSenha())
+
+    def AbrirMenu(self):
+        self.add_widget(MenuLogin())
+
+class MyApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Purple"
+        kv = Builder.load_file("telas.kv")
+        screen = kv
+        return screen
+
+
+
+if __name__ == "__main__":
+    MyApp().run()
